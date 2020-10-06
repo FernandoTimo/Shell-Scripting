11210d6be365c699ddc38e94791c01474186c586
Timoideas <Timoideas@hotmail.com>
2020-10-05 21:15:57 -0500

    si funciona

--- uninstaller.sh
+++ uninstaller.sh
@@ -1,4 +1,6 @@
 # Mostrar programas instalados
 # wmic product get name
 # Desinstalar programas Windows
-wmic product where "name like iTunes" call uninstall /nointeractive
+wmic product where "name like iTunes" call uninstall /nointeractive;
+echo "Hola"
+# f7 para ver comando recientes
